dc453752f0b27fd907444772c2e0f04416b63114
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-08-25 14:14:31 +0900

    initial commit

---
+++ emacs.d/init.el
@@ -0,0 +1,8 @@
+;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil -*-
+
+(setq user-full-name (concat ("Takahiro OKUMURA")))
+(setq user-mail-address (concat ("hfm.garden@gmail.com")))
+
+(when oldemacs-p
+  (defvar user-emacs-directory
+    (expand-file-name (concat (getenv "HOME") "/.emacs.d/"))))
